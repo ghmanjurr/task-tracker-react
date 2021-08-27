9223f6b8145d9144f058c0ccdc76313b616283ea
ghmanjurr <manjurr.ghalib@gmail.com>
2021-08-27 12:23:23 -0700

    added css to index.css from BradTraversy react crash course

--- task-tracker/src/components/Header.js
+++ task-tracker/src/components/Header.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const Header = ({ title }) => {
     return (
         <header>
-            <h1 style={headingStyle}>{title}</h1>
+            <h1>{title}</h1>
         </header>
     )
 }
@@ -17,9 +17,10 @@ Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
 
-const headingStyle = {
-    color: 'red', 
-    backgroundColor: 'black'
-}
+//CSS in js
+// const headingStyle = {
+//     color: 'red', 
+//     backgroundColor: 'black'
+// }
 
 export default Header
