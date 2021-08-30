fe5a17147766ac29bacfcb91ec6d4d04adf8cb45
ghmanjurr <manjurr.ghalib@gmail.com>
2021-08-30 15:38:04 -0700

    added .map() to tasks component

--- task-tracker/src/components/Tasks.js
+++ task-tracker/src/components/Tasks.js
@@ -21,9 +21,11 @@ const tasks = [
 
 const Tasks = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+          {tasks.map((task)=>(
+          <h3>{task.text}</h3>
+          ))}  
+        </>
     )
 }
 
