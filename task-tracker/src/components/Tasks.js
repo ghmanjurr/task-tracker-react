02ba91b9232be49d9bc377ef63b56245bf8ffafc
ghmanjurr <manjurr.ghalib@gmail.com>
2021-08-30 15:36:06 -0700

    added Tasks component

---
+++ task-tracker/src/components/Tasks.js
@@ -0,0 +1,30 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Doctors Appointment'
+        day: 'Feb 5th at 2:30 pm'
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at school'
+        day: 'Feb 6th at 1:30 pm'
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping'
+        day: 'Feb 5th at 2:30 pm'
+        reminder: false,
+    }
+]
+
+const Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tasks
